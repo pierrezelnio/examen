9b7453c7b300bdf82877cf9976dc432ab5683746
ZELNIO Pierre <zelnio.pierre@univ-catholyon.fr>
2020-03-09 14:01:43 +0100

    Première question

--- script.py
+++ script.py
@@ -2,4 +2,6 @@ print("hello World")
 print("Première modification !")
 print("Le mot cerne est masculin")
 print("7 est mon chiffre favori !")
-print("- Y a une vie après la mort ? - Non c'est l'inverse, y a une mort après la vie")
+print("- Y a une vie après la nano scort ? - Non c'est l'inverse, y a une mort après la vie")
+int("Comment tu vas ?")
+
