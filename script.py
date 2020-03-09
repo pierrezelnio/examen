6b45fe7fbcb59761f5d2e425666b2cd4eb0a9afb
ZELNIO Pierre <zelnio.pierre@univ-catholyon.fr>
2020-03-09 14:03:11 +0100

    question supplémentaire orientée culinaire

--- script.py
+++ script.py
@@ -4,4 +4,4 @@ print("Le mot cerne est masculin")
 print("7 est mon chiffre favori !")
 print("- Y a une vie après la nano scort ? - Non c'est l'inverse, y a une mort après la vie")
 int("Comment tu vas ?")
-
+int("Tu aimes le poulet au citron ?")
